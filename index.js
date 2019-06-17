b234c2a7ef895a7082160415c4de6d7cbf70d76e
jamalmaria <jamalmaria111@gmail.com>
2019-06-17 20:05:55 +0530

    UI Change

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ navigator.getBattery().then(function(batteryManager){
     var showBatteryLevel= document.getElementById("percentageRemain")
     showBatteryLevel.innerHTML = level;
 })
-// var num = n;
-// var hours = (num / 60);
-// var rhours = Math.floor(hours);
-// var minutes = (hours - rhours) * 60;
-// var rminutes = Math.round(minutes);
-// return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
-// }
 
 
 navigator.getBattery().then(function(batteryManager){
