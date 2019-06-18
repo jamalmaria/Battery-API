04b20d3925296ddce46eaa7fb5ef06d3f9209d64
jamalmaria <jamalmaria111@gmail.com>
2019-06-18 14:49:26 +0530

    Final Version

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 navigator.getBattery().then(function(battery){
+    // console.log(battery)
     var level=(battery.level*100);
     var showBatteryLevel = document.getElementById("percentage")
     showBatteryLevel.innerHTML = level;
-})
-navigator.getBattery().then(function(batteryManager){
-    var level=(batteryManager.dischargingTime);
+    var discharge = (battery.dischargingTime/3600).toFixed(2);
     var showBatteryLevel= document.getElementById("percentageRemain")
-    showBatteryLevel.innerHTML = level;
-})
-
-
-navigator.getBattery().then(function(batteryManager){
-    var level=(batteryManager.charging);
+    showBatteryLevel.innerHTML = `${discharge} Hours`;
+    var status;
+    if(battery.charging === false){
+        status = "Not Charging";
+    }else{
+        status = "Charging";
+    }
     var showBatteryLevel= document.getElementById("output")
-    showBatteryLevel.innerHTML = level;
+    showBatteryLevel.innerHTML = status;
+    var batteryIcon = document.getElementById('batteryIcon')
+    batteryIcon.style.width = `${level}%`;
 })
